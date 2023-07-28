386e14806a5006312eea16d48d84a11b0b0d8967
Druvasena Reddy <66788819+druva-06@users.noreply.github.com>
2023-07-28 20:10:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+    const firstName = document.getElementsByName('fname')[0]
+    const lastName = document.getElementsByName('lname')[0]
+    let fullName = `${firstName.value} ${lastName.value}`
+    alert(fullName)
 }
